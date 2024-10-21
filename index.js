8c2999c9103f49851cc612e752e7569404c3b0f4
Mihai-sys <mihaicostros7@gmail.com>
2024-10-21 18:28:51 +0200

    code

--- index.js
+++ index.js
@@ -62,3 +62,18 @@ editButton.addEventListener('click', async () => {
         console.log(err);
     }
 });
+
+const deleteButton = document.getElementById('form-delete-button');
+const toDeleteId = 2;
+deleteButton.addEventListener('click',  async() => {
+    try {
+    const deleteURl = `${baseUrl}${postsEndpoint}/${toDeleteId}`;
+    const response = await fetch(deleteURl, {method:'DELETE'});
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    } catch(err) {
+        console.log("err", err);
+    }
+    
+});
