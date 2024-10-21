f67f7aa022834aab61838642e7e42bb7db79d489
Mihai-sys <mihaicostros7@gmail.com>
2024-10-21 18:14:06 +0200

    fix bad code

--- index.js
+++ index.js
@@ -1,55 +1,64 @@
-const baseUrl = "https://jsonplaceholder.typeicode.com";
-const postEndpoint = "/posts";
+const baseUrl = 'https://jsonplaceholder.typicode.com';
+const postsEndpoint = '/posts';
 
-const submitButton = document.getElementById("form-submit-button");
+const submitButton = document.getElementById('form-submit-button');
 
-submitButton.addEventListener("click", async () => {
-    const form = document.getElementById("create-post-form");
+submitButton.addEventListener('click', async () => {
+    const form = document.getElementById('create-post-form');
     const formData = new FormData(form);
-    console.log("sending data: ", formData);
-    // for(const pair of formData.entries()){
+    console.log('sending data: ', formData);
+    
+    // for (const pair of formData.entries()) {
     //     console.log(pair[0], pair[1]);
     // }
 
-    const createPostUrl = baseUrl + postEndpoint;
-//     fetch(createPostUrl, {
-//         method: "POST",
-//         body: JSON.stringify(formData),
-//         headers: {
-//             "Content-Type" : "aplication/json; charset=UTF-8"
-//         }
-//     }).then(response => response.json())
-//       .then(data => console.log(data))
-//       .catch(err => console.log(err));
+    const createPostUrl = baseUrl + postsEndpoint;
+    // fetch(createPostUrl, {
+    //     method: 'POST',
+    //     body: JSON.stringify(formData),
+    //     headers: {
+    //         'Content-Type': 'aplication/json; charset=UTF-8'
+    //     }
+    // }).then(response => response.json())
+    //   .then(data => console.log(data))
+    //   .catch(err => console.log(err));
 
-    try{
-        const response = await fetch(createPostUrl,{
-            method: "POST",
+    try {
+        const response = await fetch(createPostUrl, {
+            method: 'POST',
             body: JSON.stringify(formData),
             headers: {
-                "Content-Type" : "aplication/json; charset=UTF-8"
-            }
+                'Content-Type': 'aplication/json; charset=UTF-8'
+            } 
         })
     
         const data = await response.json();
-        console.log("data", data);
-    } catch (err){
-        console.log("err", err);
-    }
-    
+        console.log('data: ', data);
+    } catch (err) {
+        console.log('err: ', err);
+    }  
 });
 
-const editButton = document.getElementById("form-edit-button");
+const editButton = document.getElementById('form-edit-button');
 const resourceId = 1;
 
-// editButton.addEventListener("click", async() => {
-//     const editUrl = baseUrl + postEndpoint + `/${resourceId}`;
+editButton.addEventListener('click', async () => {
+    const editUrl = baseUrl + postsEndpoint + `/${resourceId}`;
+    const form = document.getElementById('create-post-form');
+    const formData = new FormData(form);
+    
+    try {
+        const response = await fetch(editUrl, {
+            method: 'PUT',
+            body: JSON.stringify(formData),
+            headers: {
+                'Content-Type': 'aplication/json; charset=UTF-8'
+            } 
+        })
 
-//     try{
-//         const response = await fetch(editUrl, {
-//             method: "PUT",
-//             body: JSON.stringify(formData)
-//         })
-            
-//     }
-// });
+        const data = await response.json();
+        console.log(data);
+    } catch (err) {
+        console.log(err);
+    }
+});
